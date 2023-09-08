0ab347e05bbdf48796922818e40f359ac983961d
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-09-08 22:00:46 +0100

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Joblist from './components/Joblist';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myApp">
+      <div className='header'>
+        <div className='headerContent'>
+          <div className='headingText'>
+            <h2>devjobs</h2>
+          </div>
+          <div>
+            <i className="fa-solid fa-bars" style={{color: '#ffffff'}}></i>
+          </div>
+        </div>
+      </div>
+      <div className='joblist'>
+        <Joblist />
+      </div>
     </div>
   );
 }
